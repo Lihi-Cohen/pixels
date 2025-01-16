cedd481a9ef5887a056c0c6fbaa37f171a7a527c
dantuIl <dantu540@gmail.com>
2025-01-16 23:54:52 +0200

    Update viz_2.py

--- viz_2.py
+++ viz_2.py
@@ -1,3 +1,4 @@
+from sklearn.decomposition import PCA
 def visualize_sound_clustering(pixel_spectrograms, frame, output_path):
     """
     Visualize sound clustering by mapping log spectrograms to RGB using PCA.
